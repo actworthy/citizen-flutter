3b9d7a2eabd4d8cd3573ed5611fad6dec5c18dc6
Sam Martinez <sam.martinez.dev@gmail.com>
2018-06-01 00:31:32 -0500

    Adds reuasable app bar

---
+++ lib/ui/partials/aw_app_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// TODO: add login/logout button
+// TODO: Document
+AppBar buildAppBar(BuildContext context) {
+  return AppBar(
+    backgroundColor: Colors.purple[700],
+    title: Text('ActWorthy'),
+  );
+}
