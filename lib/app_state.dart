c11c77ad9a050da3d8bb683f9fcac45fff0bf3aa
Sam Martinez <sam.martinez.dev@gmail.com>
2018-06-22 01:56:49 -0500

    Allows fetch success and failure handling for actions from the api

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -1,5 +1,13 @@
 import 'package:actworthy_citizen/models/action.dart';
 
 class AppState {
-  List<Action> actions = [];
+  List<Action> actions;
+  bool isLoading;
+  Exception error;
+
+  AppState({
+    this.actions = const [],
+    this.isLoading = false,
+    this.error,
+  });
 }
--- lib/reducers/app_reducer.dart
+++ lib/reducers/app_reducer.dart
@@ -1,17 +1,42 @@
-import 'package:actworthy_citizen/models/action.dart';
-import 'package:actworthy_citizen/redux-actions/add_action_items_redux_action.dart';
+import 'package:actworthy_citizen/redux-actions/action_redux_actions.dart';
 import 'package:actworthy_citizen/app_state.dart';
 
 /// Aggregates all reducers
-AppState appReducers(AppState appState, dynamic action) {
-  if (action is AddActionItemsReduxAction) {
-    /// Is setting it like this going to upset Redux?
-    appState.actions = addActionItems(appState.actions, action);
-  }
+AppState appReducers(AppState appState, dynamic reduxAction) {
+  if (reduxAction is FetchActionsReduxAction)
+    return _fetchActionItems(appState, reduxAction);
+  else if (reduxAction is FetchActionsSucceededReduxAction)
+    return _fetchActionsSucceeded(appState, reduxAction);
+  else if (reduxAction is FetchActionsFailedReduxAction)
+    return _fetchActionsFailed(appState, reduxAction);
   return appState;
 }
 
-List<Action> addActionItems(
-    List<Action> items, AddActionItemsReduxAction reduxAction) {
-  return List.from(items)..addAll(reduxAction.items);
+AppState _fetchActionItems(
+    AppState appState, FetchActionsReduxAction reduxAction) {
+  // TODO: can we copy the object easily and just change the proper field?
+  return AppState(
+    actions: appState.actions,
+    isLoading: true,
+    error: null,
+  );
+}
+
+// this should have been the first call
+AppState _fetchActionsSucceeded(
+    AppState appState, FetchActionsSucceededReduxAction reduxAction) {
+  return AppState(
+    actions: List.from(appState.actions)..addAll(reduxAction.actions),
+    isLoading: false,
+    error: null,
+  );
+}
+
+AppState _fetchActionsFailed(
+    AppState appState, FetchActionsFailedReduxAction reduxAction) {
+  return AppState(
+    actions: appState.actions,
+    isLoading: false,
+    error: reduxAction.error,
+  );
 }
