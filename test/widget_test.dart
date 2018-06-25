137e305bbe5db7c2e92e775cdd0a9872bec22504
Sam Martinez <sam.martinez.dev@gmail.com>
2018-06-25 03:12:54 -0500

    Makes tests compile

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,18 @@
-// This is a basic Flutter widget test.
-// To perform an interaction with a widget in your test, use the WidgetTester utility that Flutter
-// provides. For example, you can send tap and scroll gestures. You can also use WidgetTester to
-// find child widgets in the widget tree, read text, and verify that the values of widget properties
-// are correct.
-
+import 'package:actworthy_citizen/app_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:redux/redux.dart';
 import 'package:actworthy_citizen/main.dart';
+import 'package:actworthy_citizen/constants/material_design_icons.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('App bar shows on home screen', (WidgetTester tester) async {
+    final store = Store<AppState>(null, initialState: AppState());
+
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new App());
+    await tester.pumpWidget(ActWorthyApp(store));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that the app bar is displayed.
+    expect(find.widgetWithIcon(AppBar, ActWorthyIcons.person), findsOneWidget);
   });
 }
